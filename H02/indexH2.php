3d6dca23ec54a9dc73b2c5221883d60eb357e7c1
Bink <bbavelaar@roc-dev.com>
2021-06-14 16:19:00 +0200

    H2 first commit

--- H02/indexH2.php
+++ H02/indexH2.php
@@ -4,6 +4,6 @@ $a = "Ik heb niet gefaald";
 $b = "die niet werken";
 $c = 10000;
 
-$volledigezin = $a . "Ik heb alleen" . $c . "manieren gevonden" . $b;
+$volledigezin = $a . " Ik heb alleen" . $c . " manieren gevonden" . $b;
 
 echo $volledigezin;
