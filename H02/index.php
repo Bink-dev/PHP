2986a46b6951f576d3378ca9f4ce225ec1a64d7b
Bink <bbavelaar@roc-dev.com>
2021-06-14 16:39:37 +0200

    H2 first commit

--- H02/index.php
+++ H02/index.php
@@ -7,3 +7,11 @@ $c = 10000;
 $volledigezin = $a . " ik heb alleen " . $c . " manieren gevonden " . $b;
 
 echo $volledigezin;
+
+$a = "die geen fout";
+$b = "en";
+$c = "niet.";
+
+$volledigezin = "Mens" . $b . " " . $a . $b . " Mak" . $b . ", werk" . $b . $c;
+
+echo $volledigezin;
