26cf9a8f9365bbd4b4b628c3722a9ef5752dcf57
Bink <bbavelaar@roc-dev.com>
2021-06-15 13:01:37 +0200

    H3 Apen Forloop

--- H03/index.php
+++ H03/index.php
@@ -3,3 +3,8 @@
 for ($i = 1; $i <= 9; $i++){
     echo "<img src = '../img/aapje".$i.".jpg'>";
 }
+
+for ($i = 1; $i<= 9; $i++){
+    $a = array();
+    $a = "<img src = '../img/boom".$a.".jpg'>";
+}
