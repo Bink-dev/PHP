8fafc7e42a33e0845701da11e63e9a4b2a2bb802
Bink <bbavelaar@roc-dev.com>
2021-06-15 13:57:08 +0200

    H3 bomen foreach

--- H03/index.php
+++ H03/index.php
@@ -1,10 +1,16 @@
 <?php
 
 for ($i = 1; $i <= 9; $i++){
-    echo "<img src = '../img/aapje".$i.".jpg'>";
+    echo "<img src = '../img/aapje".$i.".jpg'>" . "<Br><Br>";
 }
 
-for ($i = 1; $i<= 9; $i++){
-    $a = array();
-    $a = "<img src = '../img/boom".$a.".jpg'>";
+$bomen = array("boom1.jpg", "boom2.jpg", "boom3.jpg", "boom4.jpg", "boom5.jpg", "boom6.jpg", "boom7.jpg", "boom8.jpg", "boom9.jpg");
+
+foreach($bomen as $boom){
+    echo "<img src = '../img/" . $boom . "'>" . "<Br><Br>";
 }
+
+
+
+
+
