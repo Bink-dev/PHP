e5c68d403299ca88fd92d9973975cd157ce9839f
Bink <bbavelaar@roc-dev.com>
2021-06-21 16:29:54 +0200

    H5 Af final commit before scrum

--- H05/Opdracht5.3.php
+++ H05/Opdracht5.3.php
@@ -5,17 +5,17 @@
     <title>Formulier PHP</title>
 </head>
 <body>
-<form action="H5.3resultaat.php" method="post">
+<form action="resultaat5.3.php" method="post">
     <select name="drivers[]" multiple>
-        <option value="q1">schaap</option>
-        <option value="q2">koe</option>
-        <option value="q3">varken</option>
-        <option value="q4">konijn</option>
-        <option value="q5">kip</option>
-        <option value="q6">vos</option>
-        <option value="q7">panda</option>
-        <option value="q8">papegaai</option>
-        <option value="q9">vis</option>
+        <option value="q1">Sheep</option>
+        <option value="q2">Cow</option>
+        <option value="q3">Pig</option>
+        <option value="q4">Bunny</option>
+        <option value="q5">Chicken</option>
+        <option value="q6">Fox</option>
+        <option value="q7">Panda</option>
+        <option value="q8">Parrot</option>
+        <option value="q9">Fish</option>
     </select>
     <input type="submit" name="knop">
 </form>
