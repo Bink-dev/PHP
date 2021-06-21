6a1c60f2ab1f4b00df1615d0b705c3a61ce16d05
Bink <bbavelaar@roc-dev.com>
2021-06-21 16:20:41 +0200

    H5 opdracht 3 first commit issues with url

--- H05/index.php
+++ H05/index.php
@@ -5,7 +5,7 @@
     <title>Title</title>
 </head>
 <body>
-    <form action="resultaat.php" method="post">
+    <form action="resultaat5.1.php" method="post">
     Inlognaam <input type="text" name="inlognaam" required ><br> Wachtwoord <input type="password" name="wachtwoord" required> <input type="submit" name="knop">
     </form>
 </body>
---
+++ H05/resultaat5.3.php
@@ -0,0 +1,4 @@
+<?php
+foreach ($_POST['drivers'] as $dier) {
+    echo "<img src='../img/".$dier.".jpg'>";
+}
