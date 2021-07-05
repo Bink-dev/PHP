28522dd4bdd6bd4c4e5fe6069b26e4058ba1dcde
Bink <bbavelaar@roc-dev.com>
2021-07-05 11:52:31 +0200

    H6 final commit

--- H06/Opdracht6result.php
+++ H06/Opdracht6result.php
@@ -6,12 +6,11 @@ $pass='';
 if ($_POST['email']=="piet@worldonline.nl"&& $_POST['wachtwoord']=='doetje123'){
     echo '<h1>Welkom Piet,</h1>';
     $login=true;
-    echo $login;
 
     try {
-        $dbh = new PDO('mysql:host=localhost;dbname=phplogin;port=3306', $user, $pass);
+        $dbh = new PDO('mysql:host=localhost;dbname=phpschool;port=3306', $user, $pass);
         foreach($dbh->query('SELECT * from accounts') as $row) {
-            print_r($row);
+
         }
         $dbh = null;
     } catch (PDOException $e) {
@@ -24,12 +23,11 @@ if ($_POST['email']=="piet@worldonline.nl"&& $_POST['wachtwoord']=='doetje123'){
 if ($_POST['email']=="klaas@carpets.nl"&& $_POST['wachtwoord']=='snoepje777'){
     echo '<h1>Welkom Klaas,</h1>';
     $login=true;
-    echo $login;
 
     try {
-        $dbh = new PDO('mysql:host=localhost;dbname=phplogin; port=3306', $user, $pass);
+        $dbh = new PDO('mysql:host=localhost;dbname=phpschool; port=3306', $user, $pass);
         foreach($dbh->query('SELECT * from accounts') as $row) {
-            print_r($row);
+
         }
         $dbh = null;
     } catch (PDOException $e) {
@@ -42,12 +40,11 @@ if ($_POST['email']=="klaas@carpets.nl"&& $_POST['wachtwoord']=='snoepje777'){
 if ($_POST['email']=="truushendriks@wegweg.nl"&& $_POST['wachtwoord']=='arkiearkie201'){
     echo '<h1>Welkom Truus,</h1>';
     $login=true;
-    echo $login;
 
     try {
-        $dbh = new PDO('mysql:host=localhost;dbname=phplogin; port=3306', $user, $pass);
+        $dbh = new PDO('mysql:host=localhost;dbname=phpschool; port=3306', $user, $pass);
         foreach($dbh->query('SELECT * from accounts') as $row) {
-            print_r($row);
+
         }
         $dbh = null;
     } catch (PDOException $e) {
