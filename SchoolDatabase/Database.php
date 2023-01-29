69cbd0946c8ff596162d9e3943ac111682c7700d
binkb <bbavelaar@roc-dev.com>
2023-01-29 23:01:33 +0100

    test

--- SchoolDatabase/Database.php
+++ SchoolDatabase/Database.php
@@ -1,3 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Gegevens toevoegen aan de database van school</title>
+</head>
+<body>
+
+<div>
+    <a></a>
+</div>
+
+</body>
+</html>
+
 <?php
 $servername = "localhost";
 $database = "SchoolDatabase";
