90abb92b52b206ab15233a8adaad7681564db7bc
binkb <bbavelaar@roc-dev.com>
2023-02-12 15:16:13 +0100

    test

--- SchoolDatabase/Database.php
+++ SchoolDatabase/Database.php
@@ -1,35 +1,28 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Gegevens toevoegen aan de database van school</title>
-</head>
-<body>
-
-<div>
-    <a>Kanker dikke apen</a>
-</div>
-
-</body>
-</html>
-
 <?php
 $servername = "localhost";
-$database = "SchoolDatabase";
+$database = "schooldatabase";
 $username = "root";
 $password = "";
 
 // Create connection
-
 $conn = mysqli_connect($servername, $username, $password, $database);
 
 // Check connection
-
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
 echo ("Connection Successful");
 
-mysqli_close($conn);
+// Query variable
+$result  = $conn->query("SELECT * FROM student");
+$results = $result->fetch_all(MYSQLI_ASSOC);
+foreach($results as $key => $value){
+    DATA($value);
+}
 
+function DATA($input){
+    echo "<pre>";
+    print_r($input);
+    echo "</pre>";
+}
