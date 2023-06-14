cad556c964e2a2537871dbef0f1b2c90225ecd9e
binkb <bbavelaar@roc-dev.com>
2023-06-14 12:36:38 +0200

    test

--- SchoolDatabase/Database.php
+++ SchoolDatabase/Database.php
@@ -4,54 +4,83 @@ $dbName = 'schooldatabase';
 $username = 'root';
 $password = '';
 
-// Create a new PDO instance
+// Maak een nieuwe PDO-instantie
 $pdo = new PDO("mysql:host=$host;dbname=$dbName;charset=utf8", $username, $password);
 
-// Fetch all values from the 'name' column of the 'opleiding' table
-$query = $pdo->prepare("SELECT name FROM opleiding");
-$query->execute();
-$valuesOpleiding = $query->fetchAll(PDO::FETCH_COLUMN);
-
-// Check if the form is submitted
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $selectedColumns = [];
-    $selectedTable = 'student';
-    $code = $_POST['code'];
+    // Controleer of de vereiste formulier velden zijn ingevuld
+    if (isset($_POST['opleidingscode'])) {
+        $opleidingscode = $_POST['opleidingscode'];
 
-    // Fetch columns from 'student' table that contain the specified code
-    $query = $pdo->prepare("SHOW COLUMNS FROM $selectedTable LIKE '%$code%'");
-    $query->execute();
-    $columnsStudent = $query->fetchAll(PDO::FETCH_COLUMN);
+        // Zoek opleidingscode in de opleiding tabel
+        $query = $pdo->prepare("SELECT * FROM opleiding WHERE opleidingscode = ?");
+        $query->execute([$opleidingscode]);
+        $result = $query->fetch(PDO::FETCH_ASSOC);
 
-    // Filter the columns to include only 'roepnaam', 'tussenvoegsels', and 'achternaam'
-    $selectedColumns = array_intersect($columnsStudent, ['roepnaam', 'tussenvoegsels', 'achternaam']);
+        if ($result) {
+            $columns = array_keys($result);
 
-    // Display the columns in a list
-    echo '<h3>Selected columns:</h3>';
-    echo '<ul>';
-    foreach ($selectedColumns as $column) {
-        echo '<li>' . $column . '</li>';
+            echo '<h3>Studenten per opleiding:</h3>';
+            echo '<ul>';
+
+            // Zoek studenten met de overeenkomende opleidingscode in stu_opl
+            $query = $pdo->prepare("SELECT studentnr FROM stu_opl WHERE opleidingscode = ?");
+            $query->execute([$opleidingscode]);
+            $studentnrs = $query->fetchAll(PDO::FETCH_COLUMN);
+
+            if (!empty($studentnrs)) {
+                // Zoek de studentgegevens van de overeenkomende studentnummers
+                $query = $pdo->prepare("SELECT roepnaam, tussenvoegsels, achternaam FROM student WHERE studentnr IN (" . implode(',', $studentnrs) . ")");
+                $query->execute();
+                $studenten = $query->fetchAll(PDO::FETCH_ASSOC);
+
+                // Toon de studentgegevens in een lijst
+                foreach ($studenten as $student) {
+                    echo '<li>' . $student['roepnaam'] . ' ' . $student['tussenvoegsels'] . ' ' . $student['achternaam'] . '</li>';
+                }
+            } else {
+                echo '<li>Er zijn geen studenten gevonden voor deze opleiding.</li>';
+            }
+
+            echo '</ul>';
+
+            // Toon het formulier voor het koppelen van studenten
+            echo '<h3>Studenten koppelen aan de geselecteerde opleiding:</h3>';
+            echo '<form method="POST" action="">';
+            echo '<label for="studentnr">Studentnummer:</label>';
+            echo '<input type="text" name="studentnr" id="studentnr"><br>';
+            echo '<input type="hidden" name="opleidingscode" value="' . $opleidingscode . '">';
+            echo '<input type="submit" value="Koppelen">';
+            echo '</form>';
+        } else {
+            echo 'Geen opleiding gevonden met de opgegeven opleidingscode.';
+        }
+    } elseif (isset($_POST['studentnr']) && isset($_POST['opleidingscode'])) {
+        $studentnr = $_POST['studentnr'];
+        $opleidingscode = $_POST['opleidingscode'];
+
+        // Voeg de waarden toe aan de st_opl tabel
+        $query = $pdo->prepare("INSERT INTO stu_opl (studentnr, opleidingscode) VALUES (?, ?)");
+        $query->execute([$studentnr, $opleidingscode]);
+
+        echo 'Student succesvol gekoppeld aan de opleiding.';
+    } else {
+        echo 'Niet alle vereiste velden zijn ingevuld.';
     }
-    echo '</ul>';
-
-    // HTML form for linking students to a classroom
-    echo '<form method="POST" action="process.php">';
-    echo '<input type="hidden" name="selected_table" value="' . $selectedTable . '">';
-
-    // Generate input fields for the selected columns
-    foreach ($selectedColumns as $column) {
-        echo '<label>' . $column . ': </label>';
-        echo '<input type="text" name="' . $column . '"><br>';
-    }
-
 } else {
-    // HTML form with a dropdown menu to select values from 'opleiding' table
+    // Haal de opleidingscodes op uit de opleiding tabel
+    $query = $pdo->query("SELECT opleidingscode, naam FROM opleiding");
+    $opleidingen = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    // HTML-formulier met het dropdown-menu
+    echo '<h3>Studenten per opleiding:</h3>';
     echo '<form method="POST" action="">';
-    echo '<select name="code">';
-    foreach ($valuesOpleiding as $value) {
-        echo '<option value="' . $value . '">' . $value . '</option>';
+    echo '<label for="opleidingscode">Opleiding:</label>';
+    echo '<select name="opleidingscode" id="opleidingscode">';
+    foreach ($opleidingen as $opleiding) {
+        echo '<option value="' . $opleiding['opleidingscode'] . '">' . $opleiding['naam'] . '</option>';
     }
-    echo '</select>';
+    echo '</select><br>';
+    echo '<input type="submit" value="Bekijken">';
+    echo '</form>';
 }
-echo '<input type="submit" value="Submit">';
-echo '</form>';
--- SchoolDatabase/process.php
+++ SchoolDatabase/process.php
@@ -1,46 +1,86 @@
 <?php
-// Replace the following variables with your own database credentials
 $host = 'localhost';
 $dbName = 'schooldatabase';
 $username = 'root';
 $password = '';
 
-// Create a new PDO instance
+// Maak een nieuwe PDO-instantie
 $pdo = new PDO("mysql:host=$host;dbname=$dbName;charset=utf8", $username, $password);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Retrieve the selected table from the form
-    $selectedTable = $_POST['selected_table'];
+    // Controleer of de vereiste formulier velden zijn ingevuld
+    if (isset($_POST['opleidingscode'])) {
+        $opleidingscode = $_POST['opleidingscode'];
 
-    // Prepare the SQL query for inserting values into the selected table
-    $query = "INSERT INTO $selectedTable (";
-    $placeholders = "";
-    $values = [];
+        // Zoek opleidingscode in de opleiding tabel
+        $query = $pdo->prepare("SELECT * FROM opleiding WHERE opleidingscode = ?");
+        $query->execute([$opleidingscode]);
+        $result = $query->fetch(PDO::FETCH_ASSOC);
 
-    // Iterate through the submitted form data
-    foreach ($_POST as $key => $value) {
-        // Skip the 'selected_table' field
-        if ($key === 'selected_table') {
-            continue;
+        if ($result) {
+            $columns = array_keys($result);
+
+            echo '<h3>Studenten per opleiding:</h3>';
+            echo '<ul>';
+
+            // Zoek studenten met de overeenkomende opleidingscode in stu_opl
+            $query = $pdo->prepare("SELECT studentnr FROM stu_opl WHERE opleidingscode = ?");
+            $query->execute([$opleidingscode]);
+            $studentnrs = $query->fetchAll(PDO::FETCH_COLUMN);
+
+            if (!empty($studentnrs)) {
+                // Zoek de studentgegevens van de overeenkomende studentnummers
+                $query = $pdo->prepare("SELECT roepnaam, tussenvoegsels, achternaam FROM student WHERE studentnr IN (" . implode(',', $studentnrs) . ")");
+                $query->execute();
+                $studenten = $query->fetchAll(PDO::FETCH_ASSOC);
+
+                // Toon de studentgegevens in een lijst
+                foreach ($studenten as $student) {
+                    echo '<li>' . $student['roepnaam'] . ' ' . $student['tussenvoegsels'] . ' ' . $student['achternaam'] . '</li>';
+                }
+            } else {
+                echo '<li>Er zijn geen studenten gevonden voor deze opleiding.</li>';
+            }
+
+            echo '</ul>';
+
+            // Toon het formulier voor het koppelen van studenten
+            echo '<h3>Studenten koppelen aan de geselecteerde opleiding:</h3>';
+            echo '<form method="POST" action="">';
+            echo '<label for="studentnr">Studentnummer:</label>';
+            echo '<input type="text" name="studentnr" id="studentnr"><br>';
+            echo '<input type="hidden" name="opleidingcode" value="' . $opleidingscode . '">';
+            echo '<input type="submit" value="Koppelen">';
+            echo '</form>';
+        } else {
+            echo 'Geen opleiding gevonden met de opgegeven opleidingscode.';
         }
+    } elseif (isset($_POST['studentnr']) && isset($_POST['opleidingscode'])) {
+        $studentnr = $_POST['studentnr'];
+        $opleidingscode = $_POST['opleidingscode'];
 
-        // Append the column name to the query
-        $query .= $key . ",";
+        // Voeg de waarden toe aan de st_opl tabel
+        $query = $pdo->prepare("INSERT INTO stu_opl (studentnr, opleidingscode) VALUES (?, ?)");
+        $query->execute([$studentnr, $opleidingscode]);
 
-        // Append a placeholder for the column value
-        $placeholders .= "?,";
-        $values[] = $value;
+        echo 'Student succesvol gekoppeld aan de opleiding.';
+    } else {
+        echo 'Niet alle vereiste velden zijn ingevuld.';
     }
+} else {
+    // Haal de opleidingscodes op uit de opleiding tabel
+    $query = $pdo->query("SELECT opleidingscode, naam FROM opleiding");
+    $opleidingen = $query->fetchAll(PDO::FETCH_ASSOC);
 
-    // Remove the trailing comma from the query and placeholders
-    $query = rtrim($query, ",");
-    $placeholders = rtrim($placeholders, ",");
-
-    // Complete the SQL query and execute it with the provided values
-    $query .= ") VALUES ($placeholders)";
-    $stmt = $pdo->prepare($query);
-    $stmt->execute($values);
-
-    echo 'Values inserted successfully!';
+    // HTML-formulier met het dropdown-menu
+    echo '<h3>Studenten per opleiding:</h3>';
+    echo '<form method="POST" action="">';
+    echo '<label for="opleidingscode">Opleiding:</label>';
+    echo '<select name="opleidingscode" id="opleidingscode">';
+    foreach ($opleidingen as $opleiding) {
+        echo '<option value="' . $opleiding['opleidingscode'] . '">' . $opleiding['naam'] . '</option>';
+    }
+    echo '</select><br>';
+    echo '<input type="submit" value="Bekijken">';
+    echo '</form>';
 }
-?>
